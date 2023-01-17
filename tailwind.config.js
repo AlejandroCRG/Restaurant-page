8b9f0eff8691f80fd45e53962578807a903260ec
AlejandroCRG <alejandrocrg1@gmail.com>
2023-01-17 17:56:20 +0100

    Basic upper bar done, tailwind and webpack isntall

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    content: ['./dist/index.html'],
+    theme: {
+      extend: {
+        colors: {
+            primary: '#202225',
+            secondary: '#5865f2',
+        },
+      },
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
